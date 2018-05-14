7a5a7b82688b267c8c61446c3c413b800b6134c1
mhector <maelihector@gmail.com>
2018-05-14 13:31:07 -0500

    Add petfinder AJAX call for list of shelters close to zipcode.

---
+++ public/assets/js/keys.js
@@ -0,0 +1,8 @@
+exports.google = {
+  google_key: process.env.GOOGLE_API_KEY,
+};
+
+exports.petfinder = {
+  petfinder_api_key: process.env.API_KEY,
+  petfinder_secret_key: process.env.API_SECRET
+};
