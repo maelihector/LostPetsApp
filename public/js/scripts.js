f656ffffc1fe026db4cf793de19950a80994b858
Dylan Glover <dylanglover6@gmail.com>
2018-05-14 20:53:33 -0500

    added test script for index input

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,9 @@
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+//test of form
+$("#submitBtn").on("click", function(){
+  console.log($("#zipInput").val());
+  console.log($("#statusInput").val());
+  console.log($("#animalInput").val());
+  console.log($("#colorInput").val());
+  console.log($("#sizeInput").val());
+});
