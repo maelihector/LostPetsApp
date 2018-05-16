d31915cb9fe564ac856a564a2f611ad5c6d54089
mhector <maelihector@gmail.com>
2018-05-16 15:59:27 -0500

    Add dummy data to test GET request for lost pets map.

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO pets (animal, color, size, zip, status)
+VALUES ("cat", "white", "small", 78752, "true");
+
+INSERT INTO pets (animal, color, size, zip, status)
+VALUES ("cat", "grey", "small", 78702, "true");
+
+INSERT INTO pets (animal, color, size, zip, status)
+VALUES ("cat", "black", "small", 78704, "true");
+
+INSERT INTO pets (animal, color, size, zip, status)
+VALUES ("cat", "blue", "small", 78759, "true");
+
+INSERT INTO pets (animal, color, size, zip, status)
+VALUES ("cat", "brown", "small", 78666, "true");
