a5aa4d640faa784325a26b74122f426d9e14d9b1
mhector <maelihector@gmail.com>
2018-05-16 18:48:44 -0500

    Fix data return from 'undefined' to 'N/A' in shelters.js.

--- public/js/shelters.js
+++ public/js/shelters.js
@@ -19,30 +19,51 @@ function getShelters() {
       var shelters = res.petfinder.shelters.shelter;
       console.log(shelters);
       shelters.forEach((value) => {
-        // console.log(value.name.$t);
-        // console.log(value.address1.$t);
-        // console.log(value.city.$t);
-        // console.log(value.state.$t);
-        // console.log(value.zip.$t);
-        // console.log(value.phone.$t);
-        // console.log(value.email.$t);
+
+        // // Make sure address is valid else "N/A"
+        // function checkName(){
+        //   if (value.address1.$t === undefined){
+        //     return value.address1.$t = "N/A"
+        //   } else {
+        //     value.address1.$t = value.address1.$t;
+        //   }
+        // }
+        // checkName();
+
+        // Take returned data and save it to vars
         let name = value.name.$t;
-        let address = value.address1.$t;
+        let address;
+
+        if( value.address1.$t ){
+          address = value.address1.$t;
+        }
+        else{
+          address = "N/A";          
+        }
+
         let city = value.city.$t;
         let state = value.state.$t;
         let zip = value.zip.$t;
-        let phone = value.phone.$t;
+        let phone;
+        
+        if( value.phone.$t ){
+          address = value.phone.$t;
+        }
+        else{
+          phone = "N/A";          
+        }
         let email = value.email.$t;
-
+        
+        // Create Card for each shelter listing 
         let cardDiv = $("<div>").addClass("col s12 m6")
         let individualCard = ($("<div>").addClass("card blue-grey"));
         let cardContent = $("<div>").addClass("card-content white-text");
         let cardTitle = $("<span>").addClass("card-title").text(name);
         let p = $("<p>").text("Address: " + address + " " +city + " " + state + ", " + zip + " " + 
         "Phone: " + phone + " " + "Email: " + email);
-
+        // Dump the card with the data onto the html
         cardDiv.append(individualCard.append(cardContent.append(cardTitle).append(p)));
-
+        // Prepend to 'shelters' id DIV
         $("#shelters").prepend(cardDiv);
 
       });
