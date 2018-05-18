8762d0fd959926f4d219174dff914e37d65fc73d
mhector <maelihector@gmail.com>
2018-05-18 14:59:24 -0500

    Update to include createdAt and updatedAt.

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,25 @@
-INSERT INTO pets (animal, color, size, zip, status)
-VALUES ("cat", "white", "small", 78752, "true");
+USE pets_db;
 
-INSERT INTO pets (animal, color, size, zip, status)
-VALUES ("cat", "grey", "small", 78702, "true");
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("other", "white", "small", 78752, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, status)
-VALUES ("cat", "black", "small", 78704, "true");
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("cat", "grey", "small", 78702, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, status)
-VALUES ("cat", "blue", "small", 78759, "true");
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("dog", "black", "medium", 78704, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, status)
-VALUES ("cat", "brown", "small", 78666, "true");
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("cat", "brown", "large", 78666, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("dog", "black", "medium", 78704, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, createdAt, updatedAt)
+VALUES ("cat", "brown", "large", 78666, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
