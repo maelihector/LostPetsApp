f436514411f384f7da2bba04378e6a6b7589a04d
mhector <maelihector@gmail.com>
2018-05-19 12:36:29 -0500

    Add new seeds file with img path included.

--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,25 @@
 USE pets_db;
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("other", "Karen", "white", "small", 78752, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("other", "Samwise", "white", "small", 78742, 1, "other.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("cat", "Keni", "grey", "small", 78702, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("cat", "Keni", "grey", "small", 78702, 0, "cat2.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("dog", "Spot", "black", "medium", 78704, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("dog", "Spot", "black", "medium", 78704, 0, "dog.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("other", "Bob", "blue", "small", 78759, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("other", "Bob", "blue", "small", 78759, 1, "cat.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("cat", "Weenie", "brown", "large", 78666, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("cat", "Weenie", "brown", "large", 78666, 1, "cat.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("dog", "Rover", "black", "medium", 78704, 0, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("dog", "Rover", "black", "medium", 78704, 0, "dog2.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("other", "Lee", "blue", "small", 78759, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("other", "Lee", "blue", "small", 78759, 1, "bird.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, name, color, size, zip, lost, createdAt, updatedAt)
-VALUES ("cat", "Kitty", "brown", "large", 78666, 1, "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, name, color, size, zip, lost, img, createdAt, updatedAt)
+VALUES ("cat", "Kitty", "brown", "large", 78666, 1, "cat.jpg", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
