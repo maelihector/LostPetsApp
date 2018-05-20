ff1d9a52fb376091af98fd5f66d94bb3cdd37a76
mhector <maelihector@gmail.com>
2018-05-20 16:12:20 -0500

    Add comments and photo path for pet card testing.

--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,25 @@
 USE pets_db;
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("other", "white", "small", 78742, 1, "other.jpg", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("other", "white", "small", 78742, 1, "public/images/other.jpeg", "My horse Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("cat", "grey", "small", 78702, 0, "cat2.jpg", "mj@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("cat", "grey", "small", 78702, 0, "public/images/cat.jpg", "Winnie chased a dog into a the parking lot of XXX at around 5pm on 5/14", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("dog", "black", "medium", 78704, 0, "dog.jpg", "hector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("dog", "black", "medium", 78704, 0, "public/images/dog.jpeg", "Keni ran off on 5/20 when neighbors lit fireworks", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("other", "blue", "small", 78759, 1, "cat.jpg", "mae@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 1, "public/images/other.jpeg", "My horse Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("cat", "brown", "large", 78666, 1, "cat.jpg", "maehec@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("cat", "brown", "large", 78666, 1, "public/images/cat2.jpeg", "Winnie chased a dog into a the parking lot of XXX at around 5pm on 5/14", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("dog", "black", "medium", 78704, 0, "dog2.jpg", "mhex@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("dog", "black", "medium", 78704, 0, "public/images/dog2.jpeg", "Keni ran off on 5/20 when neighbors lit fireworks", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("other", "blue", "small", 78759, 1, "bird.jpg", "hectorm@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 1, "public/images/other.jpeg", "My bird Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
 
-INSERT INTO pets (animal, color, size, zip, lost, img, email, createdAt, updatedAt)
-VALUES ("cat", "brown", "large", 78666, 1, "cat.jpg", "mahe@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("cat", "brown", "large", 78666, 1, "public/images/cat.jpg", "Winnie chased a dog into a the parking lot of XXX at around 5pm on 5/14", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
