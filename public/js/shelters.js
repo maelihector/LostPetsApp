a54af1bd276f95f3aeb0c24acdc2fefff5f38743
mhector <maelihector@gmail.com>
2018-05-20 21:25:32 -0500

    Minor edits to pet-card formatting.

--- public/js/shelters.js
+++ public/js/shelters.js
@@ -19,46 +19,49 @@ function getShelters() {
       var shelters = res.petfinder.shelters.shelter;
       console.log(shelters);
       shelters.forEach((value) => {
-        
+
         // Take returned data and save it to vars
         let name = value.name.$t;
         let address;
 
-        if( value.address1.$t ){
+        if (value.address1.$t) {
           address = value.address1.$t;
-        }
-        else{
-          address = "N/A";          
+        } else {
+          address = "N/A";
         }
 
         let city = value.city.$t;
         let state = value.state.$t;
         let zip = value.zip.$t;
         let phone;
-        
-        if( value.phone.$t ){
+
+        if (value.phone.$t) {
           phone = value.phone.$t;
-        }
-        else{
-          phone = "N/A";          
+        } else {
+          phone = "N/A";
         }
         let email = value.email.$t;
-        
+
         // Create Card for each shelter listing 
-        let cardDiv = $("<div>").addClass("col s12 m6")
-        let individualCard = ($("<div>").addClass("card blue-grey"));
-        let cardContent = $("<div>").addClass("card-content white-text");
-        let cardTitle = $("<span>").addClass("card-title").text(name);
-        let p1 = $("<p>").text("Address:  " + address + " " +city + " " + state + ", " + zip);
-        let p2 = $("<p>").text("Phone:  " + phone + " ");
-        let p3 = $("<p>").text("Email:  " + email);
+        let cardDiv = $("<div>").addClass("col s12")
+        let card = $("<div>").addClass("card teal darken-2");
+        let cardContent = $("<div>").addClass("card-content white-text")
+          .append($("<span>").addClass("card-title").text(name))
+          .append($("<p>").text("Address:  " + address + " " + city + " " + state + ", " + zip))
+          .append($("<p>").text("Phone:  " + phone))
+          .append($("<p>").text("Email:  " + email));
+
         // Dump the card with the data onto the html
-        cardDiv.append(individualCard.append(cardContent.append(cardTitle).append(p1).append(p2).append(p3)));
+        cardDiv.append(
+          card.append(
+            cardContent
+          )
+        );
+
         // Prepend to 'shelters' id DIV
         $("#shelters").prepend(cardDiv);
-
       });
     }
   });
 }
-$("#submit").on("click", getShelters); 
+$("#submit").on("click", getShelters);
