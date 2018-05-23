a149193a823b7d3f69e7591418923f92b2b7f48f
mhector <maelihector@gmail.com>
2018-05-22 19:23:40 -0500

    Change 'createdAt' dates to test ASC/DESC function.

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,13 @@
 USE pets_db;
 
 INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("dog", "black", "medium", 78704, 0, "../images/dog2.jpg", "This dog walked into my garage yesterday (5/16) and so I took him to the shelter on 7201 Levander Loop.", "maelihector@gmail.com", "2018-03-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
 VALUES ("other", "white", "small", 78742, 1, "../images/horse.jpg", "My horse Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-04-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 1, "../images/bird.jpeg", "My bird Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-05-01 19:03:39", "2018-05-18 19:03:39");
 
 INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
 VALUES ("cat", "grey", "small", 78702, 0, "../images/cat.jpg", "Spotted this cat hanging out by the Takoba dumpsters around 6pm 5/17", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-17 19:03:39");
@@ -10,16 +16,10 @@ INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt
 VALUES ("dog", "black", "medium", 78704, 0, "../images/dog.jpg", "Snapped this picture around 4pm 5/16. Dog looked healthy", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-11 19:03:39");
 
 INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
-VALUES ("other", "blue", "small", 78759, 0, "../images/horse.jpg", "Was very surprised to see this horse walking down 51st and Berkman. I took this picture around 10 am on 5/1", "maelihector@gmail.com", "2018-05-21 19:03:39", "2018-05-18 19:03:39");
-
-INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
 VALUES ("cat", "brown", "large", 78666, 1, "../images/cat2.jpeg", "Winnie chased a dog into a the parking lot of XXX at around 5pm on 5/14", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-22 19:03:39");
 
 INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
-VALUES ("dog", "black", "medium", 78704, 0, "../images/dog2.jpg", "This dog walked into my garage yesterday (5/16) and so I took him to the shelter on 7201 Levander Loop.", "maelihector@gmail.com", "2018-03-18 19:03:39", "2018-05-18 19:03:39");
-
-INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
-VALUES ("other", "blue", "small", 78759, 1, "../images/bird.jpeg", "My bird Thanos ran away during a thunderstorm around 2pm on 5/12. He likes Apples. He is afraid of dogs.", "maelihector@gmail.com", "2018-05-01 19:03:39", "2018-05-18 19:03:39");
-
-INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
 VALUES ("cat", "brown", "large", 78666, 1, "../images/cat.jpg", "Winnie chased a dog into a the parking lot of XXX at around 5pm on 5/14", "maelihector@gmail.com", "2018-05-18 19:03:39", "2018-05-18 19:03:39");
+
+INSERT INTO pets (animal, color, size, zip, lost, img, comment, email, createdAt, updatedAt)
+VALUES ("other", "blue", "small", 78759, 0, "../images/horse.jpg", "Was very surprised to see this horse walking down 51st and Berkman. I took this picture around 10 am on 5/1", "maelihector@gmail.com", "2018-05-21 19:03:39", "2018-05-18 19:03:39");
