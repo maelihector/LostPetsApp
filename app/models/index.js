c103bd489672f212cdd3f9dad7f6a3346d3ca301
mhector <maelihector@gmail.com>
2018-05-27 18:42:22 -0500

    HD

--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,30 @@
 'use strict';
 
-var mysql     = require('mysql2');
-var fs        = require('fs');
-var path      = require('path');
+var mysql = require('mysql2');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '../config/config.json')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '../config/config.json')[env];
+var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
+  var sequelize = new Sequelize({
+    host: 'zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'f2cchicw9z1g1uru',
+    password: 'hq72cyw7idh38i5a',
+    database: 'es43okhstgyry1h9'
+  });
+};
 fs
   .readdirSync(__dirname)
   .filter(file => {
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app;
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+var app = express();
+var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./app/models");
@@ -23,23 +21,13 @@ require("./app/routes/html-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 
-// if (process.env.NODE_ENV !== 'test') {
-// 	db.sequelize.sync({ force: true }).then(function () {
-// 	  app.listen(PORT, function () {
-// 	    console.log("App listening on PORT " + PORT);
-// 	  });
-// 	});
-// }
-
-if (process.env.JAWSDB_URL) {
-	app.sequelize.sync(process.env.JAWSDB_URL);
-} else {
-	app.sequelize.sync({
-		host: 'zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'f2cchicw9z1g1uru',
-		password: 'hq72cyw7idh38i5a',
-		database: 'es43okhstgyry1h9'
+if (process.env.NODE_ENV !== 'test') {
+	db.sequelize.sync({ force: true }).then(function () {
+	  app.listen(PORT, function () {
+	    console.log("App listening on PORT " + PORT);
+	  });
 	});
-};
-app.sync();
+}
+
+
 module.exports = app;
