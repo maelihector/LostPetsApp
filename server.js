823dc0cc03972366f4bab0a9a90671a9085be138
mhector <maelihector@gmail.com>
2018-05-27 18:39:01 -0500

    HD

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app;
-var PORT = process.env.PORT || 3360;
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // Requiring our models for syncing
 var db = require("./app/models");
