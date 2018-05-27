ca2cb757890491eaeb6f3d5a09ac95682083c98e
mhector <maelihector@gmail.com>
2018-05-27 13:54:23 -0500

    Add api routes for getting animal types.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,44 +17,17 @@ module.exports = function (app) {
       .catch(err => {
         console.log(err);
         res.sendStatus(500);
-      })
+      });
   });
 
-  //View Postings With Filters
+  // Return all pets records in the database
   app.get("/api/pets", function (req, res) {
-    db.pets.findAll({
-        where: {
-          animal: req.body.animal,
-          zip: req.body.zip,
-          lost: req.body.lost,
-        }
-      }).then(data => res.json(data))
+    db.pets.findAll({})
+      .then(data => res.json(data))
       .catch(err => {
         console.log(err);
         res.sendStatus(500);
       })
-  });
-
-  // Get all pets postings
-  app.get("/api/all", function (req, res) {
-    db.pets.findAll({}).then(function (data) {
-      res.json(data);
-    }).catch(function (err) {
-      res.json(err);
-    });
-  });
-
-  // Get all pets postings in DESC order
-  app.get("/api/desc", function (req, res) {
-    db.pets.findAll({
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    }).then(function (data) {
-      res.json(data);
-    }).catch(function (err) {
-      res.json(err);
-    });
   });
 
   // View (lost === 1 (true) ) pets
@@ -83,11 +56,73 @@ module.exports = function (app) {
     });
   });
 
+    // GET all 'cat' pet records
+    app.get("/api/cats", function (req, res) {
+      db.pets.findAll({
+        where: {
+          animal: "cat"
+        }
+      }).then(function (data) {
+        res.json(data);
+      });
+    });
+  
+    // GET all 'dog' pet records
+    app.get("/api/dogs", function (req, res) {
+      db.pets.findAll({
+        where: {
+          animal: "dog"
+        }
+      }).then(function (data) {
+        res.json(data).catch(err => {
+          rest.sendStatus(500);
+        });
+      });
+    });
+  
+    // GET all 'other' pet records
+    app.get("/api/others", function (req, res) {
+      db.pets.findAll({
+        where: {
+          animal: "other"
+        }
+      }).then(function (data) {
+        res.json(data).catch(err => {
+          rest.sendStatus(500);
+        });
+      });
+    });
+
+  // Get all pets postings in DESC order
+  app.get("/api/desc", function (req, res) {
+    db.pets.findAll({
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    }).then(function (data) {
+      res.json(data);
+    }).catch(function (err) {
+      res.json(err);
+    });
+  });
+
+    // Get all pets postings in ASC order
+    app.get("/api/asc", function (req, res) {
+      db.pets.findAll({
+        order: [
+          ['createdAt', 'ASC']
+        ]
+      }).then(function (data) {
+        res.json(data);
+      }).catch(function (err) {
+        res.json(err);
+      });
+    });
+  
   // Delete a specific pet record
-  app.delete("/api/pets/:id", function (req, res) {
+  app.delete("/api/delete", function (req, res) {
     db.pets.destroy({
       where: {
-        id: req.params.id,
         email: req.body.email,
         password: req.body.password
       }
@@ -98,19 +133,4 @@ module.exports = function (app) {
     });
 
   });
-
-  // GET a specific pet record
-  app.get("/api/pets/:id", function (req, res) {
-    db.pets.findAll({
-        where: {
-          id: req.params.id
-        }
-      }).then(data => res.sendStatus(200))
-      .catch(err => {
-        console.log(data);
-        console.log(err);
-        res.sendStatus(500);
-      });
-  });
-
 }
